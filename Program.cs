794a5ff6b41724f203d688da1b8c9d6f6f68cfa0
5-LIWANBING <5-LIWANBING@5-LIWANBING>
2022-10-18 16:23:01 +0800

    22

--- Program.cs
+++ Program.cs
@@ -13,8 +13,7 @@ namespace Hydee.Auto.Interface.Set
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles() ;
-
+            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
         }
